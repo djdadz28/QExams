daa7336609b1b0c3742593152579b12c4d42c342
djdadz28 <djdadz28@gmail.com>
2020-08-17 20:33:56 +0800

    Fixed Date format in Print File

--- js/printResult.js
+++ js/printResult.js
@@ -2,7 +2,7 @@ var print = {
         id: sessionStorage.getItem('test_id'),
         first_name: sessionStorage.getItem('first_name'),
         last_name: sessionStorage.getItem('last_name'),
-        date_taken: sessionStorage.getItem('date_taken'),
+        date_taken: JSON.parse(sessionStorage.getItem('date_taken')),
         answers: JSON.parse(sessionStorage.getItem('answers')),
         critical_exam_score: sessionStorage.getItem('critical_exam_score'),
         critical_exam_result: sessionStorage.getItem('critical_exam_result'),
@@ -32,5 +32,5 @@ $('#ans_15').text("Answer: " + print.answers.question_15)
 $('#initialTest').text(print.typing_score[0] + "(" + print.typing_score[1] + "%) and " + print.ept_score )
 $('#audio_result').text(print.audio_exam_score + "/5")
 $('#written_result').text(print.critical_exam_score + "/10")
-$('#date_taken').text("Test Date: " +print.date_taken)
+$('#date_taken').text("Test Date: " + moment(print.date_taken).format('l'))
 
